74caa3b4d785b57e6fec19ebb8f2005d32da1c40
userM99 <toorroot22@gmail.com>
2026-05-20 18:53:18 +0200

    Create Grafa.sh

---
+++ Grafa.sh
@@ -0,0 +1,11 @@
+sudo apt-get update && sudo apt-get install -y \
+    firmware-amd-graphics \
+    libgl1-mesa-dri \
+    libglx-mesa0 \
+    mesa-vulkan-drivers \
+    xserver-xorg-video-radeon \
+    mesa-va-drivers \
+    vainfo \
+    libva-drm2 \
+    libva-x11-2 \
+    sqlite3
